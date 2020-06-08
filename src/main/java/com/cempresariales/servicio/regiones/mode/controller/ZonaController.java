7735d5db7c2f2d9ae70aceec0ecdbcd6964c672b
WilsonCastro1988 <wilson.castro@epn.edu.ec>
2020-06-08 13:22:48 -0500

    initial charge

--- src/main/java/com/cempresariales/servicio/regiones/mode/controller/ZonaController.java
+++ src/main/java/com/cempresariales/servicio/regiones/mode/controller/ZonaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ import com.cempresariales.servicio.commons.model.entity.Zona;
 import com.cempresariales.servicio.regiones.mode.service.ZonaServiceImp;
 
 @RestController
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS })
 @RequestMapping(value = "zona")
 public class ZonaController {
 
